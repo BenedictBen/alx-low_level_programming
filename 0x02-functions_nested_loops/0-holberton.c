395bf4b5f26be20bccb6354bb4663fd66e271292
BenedictBen <Benbenedict26@gmail.com>
2021-06-22 11:06:29 -0700

    My first commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,47 +1,49 @@
-#include "holberton.h"
+#ifndef HOLBERTON_H
+
+#define HOLBERTON_H
 
 
 
-/**
+/*
 
- * main - entry point
+ * File: holberton.h
 
- *
+ * Auth: Brennan D Baraban
 
- * Description: Prints Holberton with _putchar
+ * Desc: Header file containing declarations for all functions
 
- *
-
- * Return: always 0
+ *       used in the 0x02-functions_nested_loops directory.
 
  */
 
 
 
-int main(void)
-  
-{
-  
-  int i = 0;
-  
-  char c;
-  
-  char s[] = "Holberton\n";
-  
+int _putchar(char c);
 
-  
-  while (i <= 9)
-    
-    {
-      
-      c = s[i];
-      
-      _putchar(c);
-      
-      i++;
-      
-    }
-  
-  return (0);
-  
-}
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
