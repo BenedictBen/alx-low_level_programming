0ca8a96524624d3c7b39d8cd9823bb74c51a2433
BenedictBen <Benbenedict26@gmail.com>
2021-06-23 09:48:26 -0700

    Task Completed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -18,16 +18,10 @@ int main(void)
   
 {
   
-  char c[] = "Holberton\n";
-  
-  int i;
-  
-
-  
-  for (i = 0; i <= 9; i++)
-    
-    _putchar(c[i]);
-  
+  char c[] = "Holberton\n";  
+  int i;    
+  for (i = 0; i <= 9; i++)   
+  _putchar(c[i]);  
   return (0);
   
 }
