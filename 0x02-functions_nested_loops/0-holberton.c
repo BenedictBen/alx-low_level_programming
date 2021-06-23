45b4e672d13f26a5578b4bb38f4e3a52db919f6e
BenedictBen <Benbenedict26@gmail.com>
2021-06-23 13:57:21 -0700

    Task Completed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,27 +1,18 @@
 #include "holberton.h"
 
 
-
 /**
-
  * main - Prints a character using _putchar
-
  *
-
  * Return: 0 - Success
-
  */
 
 
-
-int main(void)
-  
-{
-  
-  char c[] = "Holberton\n";  
-  int i;    
-  for (i = 0; i <= 9; i++)   
-  _putchar(c[i]);  
-  return (0);
-  
+int main(void)  
+{  
+  char c[] = "Holberton\n"; 
+  int i;  
+  for (i = 0; i <= 9; i++)  
+    _putchar(c[i]);
+    return (0);   
 }
