fde88cc3574b4efdb4364eb1798c41ce2a30d68f
BenedictBen <Benbenedict26@gmail.com>
2021-06-24 11:06:24 -0700

    First commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+#include "holberton.h"
+
+
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+void positive_or_negative(int n)
+  
+{
+  
+  if (n > 0)
+    
+    printf("%d is positive\n", n);
+  
+  else if (n == 0)
+    
+    printf("%d is zero\n", n);
+  
+  else
+    
+    printf("%d is negative\n", n);
+  
+}
